e4c4b2c6ca375824348db1ca151a71e24a662274
D0see <317Y17@gmail.com>
2025-07-25 20:12:06 +0200

    first pass on interface

--- squirrel-for-pigs/src/App.jsx
+++ squirrel-for-pigs/src/App.jsx
@@ -9,6 +9,10 @@ import ColumnList from './components/ColumnList/ColumnList.jsx'
 
 import TableIcon from './assets/icons/Table.svg?react' 
 import BookIcon from './assets/icons/book.svg?react'
+import Database from './assets/icons/database.svg?react'
+import Flag from './assets/icons/flag.svg?react'
+import Notepad from './assets/icons/notepad-edit.svg?react'
+
 
 import testingJson from '../data/testingJson.json'
 import level1 from '../data/level1.json'
@@ -16,36 +20,34 @@ import ResultTable from './components/ResultTable/ResultTable.jsx'
 import AlertBar from './components/AlertBar/AlertBar.jsx'
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log("hey", level1)
 
   return (
-    <>
-      <QueryEntry/>
-      <br/>
-      <AlertBar state={'success'} errorMessage={''}/>
-      <br/>
-      <AlertBar state={'waiting'} errorMessage={''}/>
-      <br/>
-      <AlertBar state={'error'} errorMessage={''}/>
-      <br/>
-      <AlertBar state={'warning'} errorMessage={''}/>
-      <br/>
-      <Button text={'Submit'}/>
-      <br/>
-      <Header label={'header'} hasIcon={true} hasRightIcon={true}/>
-      <br/>
-      <Accordion Icon={BookIcon} header={'Instructions'}>
-        <p className='text-body'>{level1.instruction}</p>
-      </Accordion>
+      <div className='app-container'>
+        <div className='database-container'>
+          <Header label={'Database'} Icon={Database}/>
+          {level1.tables.map((table, index) => {
+            return <Accordion key={'table ' + index} Icon={TableIcon} header={table.tableName}>
+            <ColumnList columnDetails={table.columnDetails} columns={table.table[0]}/>
+          </Accordion>
+          })}
+        </div>
+        <div className='main-container'>
+          <Accordion Icon={BookIcon} header={'Instructions'}>
+            <p className='text-body'>{level1.instruction}</p>
+          </Accordion>
+          <Header label={'Query'} Icon={Notepad}/>
+          <div className='query-container'>
+            <QueryEntry/>
+            <div className='alert-wrapper'>
+              <AlertBar state={'waiting'} errorMessage={''}/>
+              <Button text={'Submit'}/>
+            </div>
+          </div>
+          <Header label={'Result'} Icon={Flag}/>
+          <ResultTable table={level1.tables[0].table}/>
+        </div>
+      </div>
 
-      {level1.tables.map((table, index) => {
-        return <Accordion key={'table ' + index} Icon={TableIcon} header={table.tableName}>
-        <ColumnList columnDetails={table.columnDetails} columns={table.table[0]}/>
-      </Accordion>
-      })}
-      <ResultTable table={level1.tables[0].table}/>
-    </>
   )
 }
 
--- squirrel-for-pigs/src/components/header/header.jsx
+++ squirrel-for-pigs/src/components/header/header.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import Logo from '../../assets/icons/book.svg?react'
 
 import './Header.css'
 
-export default function Header({label, hasIcon}) {
+export default function Header({label, Icon}) {
   return (
     <div className='sectionHeader'>
-        {hasIcon &&
-        <Logo className='icon'/>
-        }
+        <Icon className='icon'/>
         <h2 className='text-header label'>{label}</h2>
     </div>
   )
