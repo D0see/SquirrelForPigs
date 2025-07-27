b6e09676a4dbe231f2a840821a93f437f2292359
D0see <317Y17@gmail.com>
2025-07-27 23:52:49 +0200

    test in prod 3

--- src/features/sqlEngine/parser/sqlParser.helper.mjs
+++ src/features/sqlEngine/parser/sqlParser.helper.mjs
@@ -342,8 +342,9 @@ export const validateQueries = (sqlConsts, queryBody, whereClauses, orderByClaus
 //#region UTILS
 
 export const paramIsDirectValueRepresentation = (sqlConsts, param) => {
-    console.log("param:", param, "isDirect:", (param.startsWith('"') && param.endsWith('"')) || (param.startsWith("'") && param.endsWith("'")) || !isNaN(param) || param === dataTypes.NULL);
     const { dataTypes } = sqlConsts;
+        console.log("param:", param, "isDirect:", (param.startsWith('"') && param.endsWith('"')) || (param.startsWith("'") && param.endsWith("'")) || !isNaN(param) || param === dataTypes.NULL);
+
     return ((param.startsWith('"') && param.endsWith('"')) || (param.startsWith("'") && param.endsWith("'")) || !isNaN(param) || param === dataTypes.NULL);
 }
 
