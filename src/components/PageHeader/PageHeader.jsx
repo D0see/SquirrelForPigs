575bb0e0286df467fbb25ebcd97820a0f2824d9a
D0see <317Y17@gmail.com>
2025-08-06 11:42:56 +0200

    better column names, page header bugfix

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -3,13 +3,13 @@ import './PageHeader.css'
 
 export default function PageHeader({currStageNumber, numberOfStages}) {
   return (
-    <header class="page-header">
-        <div class="logo h2">Squirrel for Pigs</div>
+    <header className="page-header">
+        <div className="logo h2">Squirrel for Pigs</div>
 
-        <div class="game-info-container">
-            <div class="medal-icon">
+        <div className="game-info-container">
+            <div className="medal-icon">
             </div>
-            <p class="text-header stage-number">{`${currStageNumber}/${numberOfStages}`}</p>
+            <p className="text-header stage-number">{`${currStageNumber}/${numberOfStages}`}</p>
         </div>
     </header>
   )
